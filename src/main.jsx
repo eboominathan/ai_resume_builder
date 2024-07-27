1e544d919fc41b0b3a209abc8f2830ef594e2a9b
Boominathan <eboominathan@gmail.com>
2024-07-27 20:46:10 +0530

    home page created

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,24 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import SignInPage from './auth/sign-in/index.jsx'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element:<App/>
+  }, 
+  {
+    path: '/auth/sign-in',
+    element:<SignInPage/>
+  }
+  
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <RouterProvider router={router}>
     <App />
+    </RouterProvider>
   </React.StrictMode>,
 )
