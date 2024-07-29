9b16740b8e0716a342d298e78923163f46a7b43d
Boominathan <eboominathan@gmail.com>
2024-07-29 21:47:52 +0530

    theme color completed

--- src/dashboard/resume/[resumeId]/edit/components/FormSection.jsx
+++ src/dashboard/resume/[resumeId]/edit/components/FormSection.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 import PersonalDetail from './forms/PersonalDetail'
 import { Button } from '@/components/ui/button'
-import { ArrowLeft, ArrowRight, LayoutGrid } from 'lucide-react'
+import { ArrowLeft, ArrowRight, Home, LayoutGrid } from 'lucide-react'
 import Summary from './forms/Summary';
 import Experience from './forms/Experience';
 import Education from './forms/Education';
 import Skills from './forms/Skills';
-import { Navigate, useParams } from 'react-router-dom';
+import { Link, Navigate, useParams } from 'react-router-dom';
+import ThemeColor from './ThemeColor';
 
 function FormSection() {
   const [activeFormIndex,setActiveFormIndex]=useState(1);
@@ -15,10 +16,15 @@ function FormSection() {
   
   return (
     <div>
-      <div className='flex items-center justify-between'>
-        <Button variant="outline" size="sm" className="flex gap-2">
-          <LayoutGrid/> Theme</Button>
-        <div className='flex gap-5'>
+    <div className='flex items-center justify-between'>
+      <div className='flex gap-5'>
+        <Link to={"/dashboard"}>
+      <Button><Home /></Button>
+      </Link>
+      <ThemeColor/>
+     
+      </div>
+      <div className='flex gap-2'>
           {activeFormIndex> 1 && 
           <Button size="sm"
           disabled={!enableNext}
