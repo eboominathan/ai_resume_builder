7850fe3c879a083e56714c7dd86a4c93b3a8be82
Boominathan <eboominathan@gmail.com>
2024-07-30 10:55:23 +0530

    analytics updated

--- src/main.jsx
+++ src/main.jsx
@@ -54,5 +54,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
      <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
     <RouterProvider router={router} />
     </ClerkProvider>
+    <Analytics/>
   </React.StrictMode>,
 )
